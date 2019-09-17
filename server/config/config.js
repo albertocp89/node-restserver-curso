d7486883c1bda1af0fbe51c19ecf1899cd2c10eb
Alberto <albertocp89@hotmail.com>
2019-09-17 14:35:12 +0200

    RESTServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,15 @@
 //Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+//Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//Base de datos
+let urlDB;
+if(process.env.NODE_ENV === 'dev'){
+    urlDB='mongodb://localhost:27017/cafe';
+}else{
+    urlDB='mongodb+srv://eilbertocp89:wZ5XqfNrJI6sP0li@cluster0-mrwr7.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
